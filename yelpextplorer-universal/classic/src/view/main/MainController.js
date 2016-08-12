87a71e04c245235bc80ac9c3fbb943a6f5b7204a
Lee Boonstra <lee.boonstra@gmail.com>
2016-08-12 14:14:36 +0200

    minor fixes

--- yelpextplorer-universal/classic/src/view/main/MainController.js
+++ yelpextplorer-universal/classic/src/view/main/MainController.js
@@ -1,58 +1,5 @@
 Ext.define('YelpExtplorer.view.main.MainController', {
     extend: 'YelpExtplorer.view.main.MainControllerShared',
-    alias: 'controller.main',
-
-    requires: ['Shared.Geocode'],
-
-    initViewModel: function(vm) {
-        var me = this;
-
-        // When the user types, wait a second before updating {city}
-        vm.bind('{typedCity}', Ext.Function.createBuffered(function(city) {
-            vm.set('city', city);
-        }, 1000), me);
-        // When the user types, wait a second before updating {category}
-        vm.bind('{typedCategory}', Ext.Function.createBuffered(function(category) {
-            vm.set('category', category);
-        }, 1000));
-
-        // Set the initial typed values.
-        vm.set('typedCity', vm.get('city'));
-        vm.set('typedCategory', vm.get('category'));
-
-        vm.bind('{city}', me.geocodeCity, me);
-
-        vm.bind('{location}', me.load, me);
-        vm.bind('{category}', me.load, me);
-
-    },
-
-
-    geocodeCity: function (city) {
-        var me = this;
-        Shared.Geocode.geocodeAddress(city, function (coordinates) {
-            if (coordinates) {
-                me.getViewModel().set('location', coordinates);
-            }
-        });
-    },
-
-    load: function() {
-        var vm = this.getViewModel();
-        var location = vm.get('location');
-        var category = vm.get('category');
-        if (!location) return;
-        var store = vm.getStore('businesses');
-        var params = {
-            ll: (location.latitude + ',' + location.longitude)
-        };
-        if (category) {
-            params.term = category;
-        }
-        store.load({
-            params: params
-        });
-        vm.set('business', null);
-    }
+    alias: 'controller.main'
 
 });
--- yelpextplorer-universal/classic/src/view/main/MainModel.js
+++ yelpextplorer-universal/classic/src/view/main/MainModel.js
@@ -2,24 +2,4 @@ Ext.define('YelpExtplorer.view.main.MainModel', {
     extend: 'YelpExtplorer.view.main.MainModelShared',
     alias: 'viewmodel.main',
 
-    requires:['YelpExtplorer.model.Business'],
-
-    data: {
-        location: null,
-        business: null,
-        typedCity: null,
-        typedCategory: null,
-        category: 'pizza',
-        city: 'Amsterdam'
-    },
-    stores: {
-        businesses: { 
-            model: 'YelpExtplorer.model.Business',
-            sorters: ['name'],
-            pageSize: 20 // Yelp limits results to 20 at a time
-        },
-        sortableBusinesses: {
-            source: '{businesses}'
-        }
-    }
 });
